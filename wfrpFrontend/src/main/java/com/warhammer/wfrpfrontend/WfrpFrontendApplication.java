dd1c7f7e5b7d9ae4180d1739faed307867e19c8c
enmebaraggesi <koniec.poczatek@gmail.com>
2023-10-13 14:03:24 +0200

    0.01 project initialization

---
+++ wfrpFrontend/src/main/java/com/warhammer/wfrpfrontend/WfrpFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.warhammer.wfrpfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfrpFrontendApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(WfrpFrontendApplication.class, args);
+    }
+    
+}
---
+++ wfrpFrontend/src/test/java/com/warhammer/wfrpfrontend/WfrpFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warhammer.wfrpfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WfrpFrontendApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
